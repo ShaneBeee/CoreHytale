024eb219b152b8079d4afbe8532e5676b778ef00
ShaneBeee <shanebolenback@me.com>
2026-01-15 08:54:40 -0800

    TimeCommand - update to use as a world command

--- src/main/java/com/shanebeestudios/core/plugin/commands/TimeCommand.java
+++ src/main/java/com/shanebeestudios/core/plugin/commands/TimeCommand.java
@@ -1,11 +1,9 @@
 package com.shanebeestudios.core.plugin.commands;
 
-import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
-import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractWorldCommand;
 import com.hypixel.hytale.server.core.modules.time.WorldTimeResource;
-import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.shanebeestudios.core.api.util.Utils;
@@ -15,21 +13,23 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
-public class TimeCommand extends AbstractPlayerCommand {
+public class TimeCommand extends AbstractWorldCommand {
 
     protected TimeCommand() {
         super("now", "Prints the current time in a nicer format.");
     }
 
-
     @Override
-    protected void execute(@NotNull CommandContext commandContext, @NotNull Store<EntityStore> store, @NotNull Ref<EntityStore> ref, @NotNull PlayerRef playerRef, @NotNull World world) {
+    protected void execute(@NotNull CommandContext commandContext, @NotNull World world, @NotNull Store<EntityStore> store) {
         WorldTimeResource worldTimeResource = store.getResource(WorldTimeResource.getResourceType());
         Instant gameTime = worldTimeResource.getGameTime();
-        String format = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss")
-            .withZone(ZoneId.systemDefault())
+
+        String pausedMessage = world.getWorldConfig().isGameTimePaused() ? "paused" : "unpaused";
+        String name = world.getName();
+        String format = DateTimeFormatter.ofPattern("u/MM/dd h:mm:ss a")
+            .withZone(ZoneId.of("UTC"))
             .format(gameTime);
-        Utils.sendMessage(playerRef, "The time is " + format);
+        Utils.sendMessage(commandContext.sender(), "The [" + pausedMessage + "] time in '" + name + "' is " + format);
     }
 
 }
