07d149b5ccb490430ec3c97866ab6e9767fa3f02
ShaneBeee <shanebolenback@me.com>
2026-01-16 14:36:24 -0800

    build.gradle.kts - changed file location again

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation(files("/Users/ShaneBee/Desktop/Server/Hytale/Creative/HytaleServer.jar"))
+    implementation(files("/Users/ShaneBee/Desktop/Server/Hytale/Assets/HytaleServer.jar"))
     implementation("org.jetbrains:annotations:26.0.2")
 }
 
