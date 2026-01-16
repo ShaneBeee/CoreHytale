910433fcc0e77124d77abf229498528bc64672b9
ShaneBeee <shanebolenback@me.com>
2026-01-16 09:23:10 -0800

    CorePlugin - change log method

--- src/main/java/com/shanebeestudios/core/plugin/CorePlugin.java
+++ src/main/java/com/shanebeestudios/core/plugin/CorePlugin.java
@@ -2,10 +2,9 @@ package com.shanebeestudios.core.plugin;
 
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.shanebeestudios.core.api.util.Utils;
 import com.shanebeestudios.core.plugin.commands.CommandHandler;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
-
-import java.util.logging.Level;
 
 @SuppressWarnings("unused")
 public class CorePlugin extends JavaPlugin {
@@ -19,16 +18,12 @@ public class CorePlugin extends JavaPlugin {
 
     @Override
     protected void setup() {
-        log("Setting up CorePlugin...");
+        Utils.log("Setting up CorePlugin...");
         CommandHandler.registerCommands(getCommandRegistry());
     }
 
     @Override
     protected void start() {
-    }
-
-    private void log(String message) {
-        getLogger().at(Level.INFO).log(message);
     }
 
     public static CorePlugin getInstance() {
