ee2eb6624a60e1f68ac26f402813a09a66f3d39d
ShaneBeee <shanebolenback@me.com>
2026-01-18 10:58:02 -0800

    ClearChatcommand - add new command to clear chat

---
+++ src/main/java/com/shanebeestudios/core/plugin/commands/ClearChatcommand.java
@@ -0,0 +1,27 @@
+package com.shanebeestudios.core.plugin.commands;
+
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import org.jetbrains.annotations.NotNull;
+
+public class ClearChatcommand extends AbstractPlayerCommand {
+
+    public ClearChatcommand() {
+        super("clearchat", "Clears the chat");
+        requirePermission("core.commands.clearchat");
+    }
+
+    @Override
+    protected void execute(@NotNull CommandContext commandContext, @NotNull Store<EntityStore> store, @NotNull Ref<EntityStore> ref, @NotNull PlayerRef playerRef, @NotNull World world) {
+        for (int i = 0; i < 50; i++) {
+            playerRef.sendMessage(Message.raw(" "));
+        }
+    }
+
+}
